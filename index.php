a8ade508e519868a675b90243dac484ef19ec8b4
Sean A <your_email@example.com>
2025-10-02 18:18:03 +0800

    Added index.php, db.php, and css

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Simple PHP test
+echo "<h1>Welcome to NewQueue 🚀</h1>";
+echo "<p>Current date and time: " . date("Y-m-d H:i:s") . "</p>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
